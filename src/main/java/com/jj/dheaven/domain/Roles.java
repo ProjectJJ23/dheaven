f1ed8e86a06296e528cf366d89ba023f090a8464
DESKTOP-B2SS329\Jin <jinilove2312@gmail.com>
2024-02-16 22:25:25 +0900

    0216-3

--- src/main/java/com/jj/dheaven/domain/Roles.java
+++ src/main/java/com/jj/dheaven/domain/Roles.java
@@ -2,6 +2,6 @@ package com.jj.dheaven.domain;
 
 public enum Roles {
         MEMBER, ADULT_MEMBER, ADMIN
-        // ADULT_MEMBER 성인만 술 예약 서비스 이용가능2
+        // ADULT_MEMBER 성인만 술 예약 서비스 이용가능22222
 
 }
